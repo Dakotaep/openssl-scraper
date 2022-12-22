0dce97ff20fc54b86a3da0dc8dde963e9a8c65ec
= <dk@gmail.com>
2022-12-22 15:33:56 -0500

    Adding openssl binaries and download-log

--- openssl-scraper.py
+++ openssl-scraper.py
@@ -12,7 +12,6 @@ def getVersion(url_text):
     if m:
         found = m.group(1)
         return found
-
 ARCH = '-x64'
 url = 'https://git.openssl.org'
 url_tags = 'https://git.openssl.org/?p=openssl.git;a=tags'
@@ -25,9 +24,13 @@ f = open("Download-log.txt", "w")
 for i in range(len(urls)):
     url_href = urls[i].get('href')
     url_text = urls[i].text
-    # Filter releases that are not FIPS
+
     openssl_release = 'a=tag' in url_href and 'release tag' in url_text and 'FIPS' not in url_text
+    
+    # Test specific version
+    # openssl_release = openssl_release and (' 1.0' in url_text or ' 0.' in url_text)
     if openssl_release:
+        print(url_text)
         try:
             commit = url_href.split("h=",1)[1]
             snapshot = f'https://git.openssl.org/?p=openssl.git;a=snapshot;h={commit};sf=tgz'
@@ -39,18 +42,31 @@ for i in range(len(urls)):
             t.close()
             
             # Build and extract static built openssl binary
-            build_directory = os.path.dirname(os.path.realpath(__file__)) + '/'  + getVersion(url_text) + ARCH +'/'
-            full_path = build_directory + '/' + directory_name + '/'
-            subprocess.run([full_path + 'Configure', 'no-shared',  'CFLAGS=-g', 
-                            f'--prefix={build_directory}', f'--openssldir={build_directory}'], cwd=full_path)
-            subprocess.run(['make'], cwd=full_path, shell=True)
-            subprocess.run(['cp', full_path + 'apps/openssl', build_directory ] )
+            if ' 1.' in url_text or ' 0.' in url_text:
+                build_directory = os.path.dirname(os.path.realpath(__file__)) + '/'  + getVersion(url_text) + ARCH +'/'
+                full_path = build_directory + '/' + directory_name + '/'
+                if ' 1.0' in url_text or ' 0.' in url_text:
+                    subprocess.run([full_path + 'config', 'no-shared', 
+                                    f'--prefix={build_directory}', f'--openssldir={build_directory}'], cwd=full_path)
+                if ' 1.' in url_text:
+                    subprocess.run([full_path + 'config', 'no-shared',  'CFLAGS=-g', 
+                                    f'--prefix={build_directory}', f'--openssldir={build_directory}'], cwd=full_path)
+                subprocess.run(['make'], cwd=full_path, shell=True)
+                subprocess.run(['cp', full_path + 'apps/openssl', build_directory ] )
+            else:
+                build_directory = os.path.dirname(os.path.realpath(__file__)) + '/'  + getVersion(url_text) + ARCH +'/'
+                full_path = build_directory + '/' + directory_name + '/'
+                subprocess.run([full_path + 'Configure', 'no-shared',  'CFLAGS=-g', 
+                                f'--prefix={build_directory}', f'--openssldir={build_directory}'], cwd=full_path)
+                subprocess.run(['make'], cwd=full_path, shell=True)
+                subprocess.run(['cp', full_path + 'apps/openssl', build_directory ] )
 
             # Remove directory
             subprocess.run(["rm", "-R", full_path])
-            f.write(url_text + ' - PASSED')
+            f.write(url_text + ' - PASSED\n')
         except:
-            f.write(url_text + ' - FAILED')
+            f.write(url_text + ' - FAILED\n')
+
 f.close()
 
 
